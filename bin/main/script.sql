1b49ccf3807876c8aafd4fc76dd9effb6560ff93
BOUHAROUD <Charif.BOUHAROUD@gcatrans.com>
2019-09-23 15:05:03 +0200

    Premiere essai

---
+++ bin/main/script.sql
@@ -0,0 +1,7 @@
+--select * from ADM_PIECEC_LIGNE;
+
+
+select * ADM_PIECEC_LIGNE;
+
+
+--ALTER TABLE ADM_PIECEC_LIGNE;
---
+++ src/main/resources/fileExtensions.sql
@@ -0,0 +1,6 @@
+--select * from ADM_PIECEC_LIGNE;
+
+
+select * ADM_PIECEC_LIGNE;
+
+
